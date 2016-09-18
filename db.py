394849255bdcb8fc4784eed59afadce07e943e6a
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-09-17 20:33:44 -0400

    more untested changes

---
+++ db.py
@@ -0,0 +1,28 @@
+import psycopg2
+import random
+def isCorrectCode(name, code):
+    conn = psycopg2.connect(database='bank',  host='localhost', port=26257)
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    cur.execute("select code, ascii from mini where ascii = "+name+" ; ")
+    row = cur.fetchall()[0]
+    correct = row[0]
+    cur.close()
+    conn.close()
+    return correct==code
+
+
+def getRandomLetter():
+    conn = psycopg2.connect(database='bank',  host='localhost', port=26257)
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    random.seed()
+    num = random.randrange(1,37)
+    cur.execute("select id,spelling from words where id = "+num+" ; ")
+
+    row = cur.fetchall()[0]
+    spell=row[1]
+
+    cur.close()
+    conn.close()
+    return spell
