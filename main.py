930661b062000eb157df236165efaa276c2e6c29
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-09-17 23:58:23 -0400

    works works works works

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python
-from threading import Thread
-import time
-from db import *
-import signal
-import sys
+
+from threading import Thread,Event
 import time
 import os
+import sys
+import signal
+from db import *
+import serial #pip install pySerial
 
-def signal_handler(signal, frame):
-    print 'Aah! Too coward to know if you\'re a cat, eh?'
-    os._exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-def arduinoSerilizer(buffer):
-	import serial #pip install pySerial
-	#serializer = serial.Serial('COM3', 9600) #/dev/tty.usbserial, is used on Linux
+def arduinoSerilizer(buffer,event):
 	serializer = serial.Serial('/dev/tty.usbmodem1421', 9600) #/dev/tty.usbserial, is used on Linux
 
-	while 1:
+	while event.is_set():
 		buffer.append(serializer.readline()) #Use this command anytime you want to get an update
+	else:
+		serializer.close()
 
-def translate(rawData):
-	#Query database for rawData
-	translatedData = 5#query
-	return translatedData
+# def translate(rawData):
+# 	#Query database for rawData
+# 	translatedData = 5#query
+# 	return translatedData
 
 def log(transData):
 	#Insert into table datetime and transData
@@ -32,30 +27,40 @@ def log(transData):
 
 
 buffer = []
-thread = Thread(target=arduinoSerilizer, args=(buffer,))
+event=Event()
+event.set()
+thread = Thread(target=arduinoSerilizer, args=(buffer,event))
+thread.daemon = True
 thread.start()
 
 #DO your stuff here
-print "Welcome to Morse Code cats! Figure out the morse code to see which cat you are!"
-#hile 1:
 letter = getRandomLetter()
 print("What is the correct morse code for '"+letter+"' ?\n")
+
 try:
-	x = buffer.pop(0)
-
-	if(x[-1]=='\n'):
-		code=x[:-1]
-	else:
-		code=x
-
+	while 1:
+		try:
+			x = buffer.pop(0)
+			break
+		except:
+			pass
+	if x[-1] == '\r' or x[-1] == '\n':
+		code = x[:-1]
+	if code[-1] == '\r' or code[-1] == '\n':
+		code = code[:-1]
 	if(isCorrectCode(letter,code)):
-		print "You are a CAT! Try again or Press Ctrl+C to quit"
+		print "You are a CAT!"
 	else:
 		print "You're not a cat. Go *** yourself"
-		os._exit(1)
+		#os._exit(1)
+	#print code
+	#x2 = translate(x)
+	#log(x2)
 
-	time.sleep(1)
-except:
-	pass
-	#print("OOPs error!")
-	#sys.exit(2)
+	#print("Received: {}, Translated: {}".format(code, code))
+	event.clear()
+	#thread.join()
+	print "woo"
+	sys.exit()		#time.sleep(1)
+except Exception as e:
+	print str(e)
