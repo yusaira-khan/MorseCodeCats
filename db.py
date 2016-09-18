4866877e1a30f885484f95df8608fc5d580827f1
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-09-17 20:49:50 -0400

    tested script

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import psycopg2
 import random
 def isCorrectCode(name, code):
-    conn = psycopg2.connect(database='bank',  host='localhost', port=26257)
+    conn = psycopg2.connect(database='morsecode', user='root', host='localhost', port=26257)
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    cur.execute("select code, ascii from mini where ascii = "+name+" ; ")
+    cur.execute("select code, ascii from mini where ascii = '"+name+"' ; ")
     row = cur.fetchall()[0]
     correct = row[0]
     cur.close()
@@ -13,12 +13,12 @@ def isCorrectCode(name, code):
 
 
 def getRandomLetter():
-    conn = psycopg2.connect(database='bank',  host='localhost', port=26257)
+    conn = psycopg2.connect(database='morsecode', user='root', host='localhost', port=26257)
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     random.seed()
     num = random.randrange(1,37)
-    cur.execute("select id,spelling from words where id = "+num+" ; ")
+    cur.execute("select id,spelling from words where id = "+str(num)+" ; ")
 
     row = cur.fetchall()[0]
     spell=row[1]
@@ -26,3 +26,13 @@ def getRandomLetter():
     cur.close()
     conn.close()
     return spell
+
+def test():
+    let= getRandomLetter()
+    code = raw_input("what is the correct morse code for '"+let+"' ?\n")
+    if(isCorrectCode(let,code)):
+        print("correct")
+    else:
+        print "incorrect"
+
+test()
